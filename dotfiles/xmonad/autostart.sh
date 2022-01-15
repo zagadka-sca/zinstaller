58b0d45b0b380ec54fcaaff16e85a507bf750922
Stefan Calaci <scalaci@icebergdatalab.com>
2022-01-15 17:13:13 +0100

    wip details

--- dotfiles/xmonad/autostart.sh
+++ dotfiles/xmonad/autostart.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
-$HOME/.scripts/default.sh
-lxsession 
+#$HOME/.scripts/default.sh
+#lxsession 
 nitrogen --restore &
 picom &
 volumeicon &
 nm-applet &
-trayer --edge top --align right --widthtype request --padding 6 --SetDockType true --SetPartialStrut true --expand true --monitor 0  --transparent true --alpha 0 --tint 0x282c34  --height 28 &
+blueman-applet &
+trayer --edge top --align right --widthtype request --padding 6 --SetDockType true --SetPartialStrut true --expand true --monitor 0  --transparent true --alpha 0 --tint 0x282c34  --height 16 &
 gnome-screensaver &
-xautolock -time 10 -locker 'gnome-screensaver-command -l' &
-~/.local/bin/g810-led -dp c33c -p ~/.config/logitech-keyboard.profile
+xautolock -time 10 -locker 'slock' &
+#~/.local/bin/g810-led -dp c33c -p ~/.config/logitech-keyboard.profile
 LG3D
---
+++ scripts/standalone-precision.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1 --mode 3840x2400 --scale 1x1 --dpi 290 
