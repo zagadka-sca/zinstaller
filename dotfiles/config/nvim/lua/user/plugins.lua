5c7847ca559ac436dc2101c317d2c4b333c6a0b2
Stefan Calaci <scalaci@icebergdatalab.com>
2022-01-23 16:14:26 +0100

    screen wip, todo, nvim

--- dotfiles/config/nvim/lua/user/plugins.lua
+++ dotfiles/config/nvim/lua/user/plugins.lua
@@ -99,6 +99,7 @@ return packer.startup(function(use)
   use "tpope/vim-fugitive"  
   use "tpope/vim-surround"
 
+  use "chrisbra/csv.vim"
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
--- scripts/home.sh
+++ scripts/home.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-xrandr --output eDP-1-1 --mode 3840x2400 --scale 0.7x0.7 --pos 3840x0 --rotate normal --output DP-1-1 --off --output DP-1-2 --off --output DP-1-3 --off --output DP-1-3-1 --mode 3840x2160 --scale 0.7x0.7 --pos 0x0 --rotate normal --output DP-1-3-2 --off --output DP-1-3-3 --off
+xrandr --dpi 96 --output DP-1 --mode 3840x2160 --scale 1x1  --output eDP-1 --mode 3840x2400 --scale 1x1 --right-of DP-1
+
+
